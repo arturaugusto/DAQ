83f272d9e21a1d4b298abd0a629c9a020061bed3
lightingghost <zhenp3ngzhou@gmail.com>
2015-08-01 22:34:16 -0700

    use b'string' instead of  'string'
    in Python 3
    
    Finally Works

--- DAQ.py
+++ DAQ.py
@@ -10,8 +10,8 @@ class currentInput(Task):
     def __init__(self):
         Task.__init__(self)
         self.data = numpy.zeros(10)
-        self.CreateAICurrentChan( "cDAQ1Mod4/ai0",
-                                  "",
+        self.CreateAICurrentChan( b'cDAQ1Mod4/ai0',
+                                  b'',
                                   DAQmx_Val_Cfg_Default,
                                   -0.02, 0.02,
                                   DAQmx_Val_Amps,
@@ -19,7 +19,7 @@ class currentInput(Task):
                                   249.0,
                                   None
                                   )
-        self.CfgSampClkTiming('',10000.0,DAQmx_Val_Rising, DAQmx_Val_ContSamps,20)
+        self.CfgSampClkTiming(b'',10000.0,DAQmx_Val_Rising, DAQmx_Val_ContSamps,20)
         self.AutoRegisterEveryNSamplesEvent(DAQmx_Val_Acquired_Into_Buffer,10,0)
         self.AutoRegisterDoneEvent(0)
 
@@ -40,7 +40,7 @@ class voltageOutput(Task):
 
     def __init__(self):
         Task.__init__(self)
-        self.CreateAOVoltageChan('cDAQ1Mod1/ao2','',0.0,10.0,DAQmx_Val_Volts,None)
+        self.CreateAOVoltageChan(b'cDAQ1Mod1/ao2',b'',0.0,10.0,DAQmx_Val_Volts,None)
 
     def voltOut(self,voltage):
         data = numpy.array([voltage,voltage],dtype = numpy.float64)
@@ -48,14 +48,14 @@ class voltageOutput(Task):
 
 
 if __name__ == '__main__':
-    #current_input = currentInput()
-    #current_input.StartTask()
+    current_input = currentInput()
+    current_input.StartTask()
 
     voltage_output = voltageOutput()
-    voltage_output.voltOut(2.0)
-    #voltage_output.StopTask()
+    voltage_output.voltOut(4.0)
+    voltage_output.StopTask()
 
-    #print(current_input.getResult)
-    #current_input.StopTask()
+    print(current_input.getResult())
+    current_input.StopTask()
 
 
